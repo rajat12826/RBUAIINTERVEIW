e2c7986686314d0a563c2a05878dbd7ce0b3b091
rajat12826 <155458603+rajat12826@users.noreply.github.com>
2024-12-27 18:27:22 +0530

    Update supabaseClients.js

--- src/utils/supabaseClients.js
+++ src/utils/supabaseClients.js
@@ -6,7 +6,7 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseKey = process.env.NEXT_PUBLIC_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
-console.log(supabase);
+
 
 
 // export default supabaseClients
